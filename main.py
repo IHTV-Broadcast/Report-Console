63d61a322187f753d90ca68e59380749e5371c49
Broadcast Department <Broadcast@imamhusseintv.com>
2026-08-30 01:37:58 +0300

    access update

--- main.py
+++ main.py
@@ -2645,4 +2645,4 @@ app.mount("/", StaticFiles(directory=static_dir, html=True), name="static")
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=3006, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=3006, reload=True)
